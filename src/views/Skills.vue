2d6ad598b8ed8eb8c6b7f85bdd9ff552a3161221
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-11-17 22:28:30 +0100

    Added Skills section

---
+++ src/views/Skills.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul
+    class="mb-48 w-2/3 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 text-white fira-code"
+  >
+    <li
+      v-for="(desc, skill) in skills"
+      :key="skill"
+      :class="skill != 0 ? 'mt-8' : ''"
+    >
+      <div class="flex">
+        <div class="flex-shrink-0">
+          <div class="flex items-center justify-center h-12 w-12 bg-redish-500">
+            <!-- Heroicon name: globe-alt -->
+            <svg
+              class="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+              />
+            </svg>
+          </div>
+        </div>
+        <div class="ml-4">
+          <h4 class="text-lg leading-6 font-bold">
+            {{ skill }}
+          </h4>
+          <p class="mt-2 text-base leading-6 text-gray-500">
+            {{ desc }}
+          </p>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skills: {
+        "Web Development":
+          "lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum ",
+        "Artificial Intelligence":
+          "lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum ",
+        "Graphic Design":
+          "lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum ",
+        "Desktop Development":
+          "lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum lorem picsum ",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
