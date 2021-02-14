cd5c06318615de8a359aab8f0e62661880daf3b7
Chems Eddine Senoussi <chemsedd@outlook.fr>
2021-02-14 23:53:26 +0100

    Updated About me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,20 @@
 <template>
   <div
     id="about"
-    class="min-h-screen flex items-center justify-center my-10 sm:my-0"
+    class="min-h-screen flex flex-col items-center justify-center my-10 sm:my-0
+    w-full gap-y-10"
   >
+    <c-title>About Me</c-title>
     <!-- data-aos="fade-up" -->
     <div
       class="flex items-center justify-center w-10/12 shadow-2xl bg-gray-100 rounded-3xl"
+      data-aos="fade"
     >
       <!-- My bio -->
       <div
         class="px-10 grid grid-cols-1 sm:grid-cols-3 gap-y-10 sm:gap-x-4 items-center text-dark  my-24"
       >
         <div class="sm:col-span-2 flex flex-col flex-1">
-          <div class="text-4xl sm:text-6xl font-bold">
-            <span class="underline text-redish-500 px-4 rounded-lg">#</span>
-            About Me
-          </div>
           <div class="pt-4 leading-7 text-gray-800 fira-code">
             I am <span class="key-word">Chems Eddine Senoussi</span>, a Computer
             Science graduate with Master's degree in
@@ -28,39 +27,41 @@
           </div>
         </div>
         <!-- Infos about me -->
-        <div class="justify-center items-center hidden">
-          <ul class="grid grid-cols-2">
-            <dt><v-icon class="listed-icon" name="birthday-cake" /></dt>
+        <ul class="flex flex-col gap-y-2">
+          <li>
+            <v-icon class="listed-icon" name="birthday-cake" />
+            <span class="key-word"> October 1<sup>st</sup>, 1995. </span>
+          </li>
+
+          <li>
+            <v-icon class="listed-icon" name="globe-africa" />
+            <span class="key-word"> Algerian. </span>
+          </li>
+
+          <li>
+            <v-icon class="listed-icon" name="home" />
+            <span class="key-word"> Biskra, Algeria. </span>
+          </li>
+
+          <dl class="flex gap-x-4">
+            <dt>
+              <v-icon class="listed-icon" name="language" />
+            </dt>
             <dd>
-              <span class="key-word"> October 1<sup>st</sup>, 1995. </span>
+              <ul>
+                <li>
+                  <span class="key-word"> Arabic (native) </span>
+                </li>
+                <li>
+                  <span class="key-word"> English (Fluent) </span>
+                </li>
+                <li>
+                  <span class="key-word"> French (Fluent) </span>
+                </li>
+              </ul>
             </dd>
-
-            <dt><v-icon class="listed-icon" name="globe-africa" /></dt>
-            <dd><span class="key-word"> Algerian. </span></dd>
-
-            <dt><v-icon class="listed-icon" name="home" /></dt>
-            <dd><span class="key-word"> Biskra, Algeria. </span></dd>
-
-            <dl class="flex gap-x-4">
-              <dt>
-                <v-icon class="listed-icon" name="language" />
-              </dt>
-              <dd>
-                <ul>
-                  <li>
-                    <span class="key-word"> Arabic (native) </span>
-                  </li>
-                  <li>
-                    <span class="key-word"> English (Fluent) </span>
-                  </li>
-                  <li>
-                    <span class="key-word"> French (Fluent) </span>
-                  </li>
-                </ul>
-              </dd>
-            </dl>
-          </ul>
-        </div>
+          </dl>
+        </ul>
       </div>
       <a href="#app" class="page-up" data-aos="fade-up">
         <v-icon name="caret-up" class="w-6 h-6" />
@@ -69,11 +70,13 @@
   </div>
 </template>
 <script>
+import CTitle from "../components/Title.vue";
 import Experience from "../components/Experience.vue";
 
 export default {
   name: "About",
   components: {
+    CTitle,
     Experience,
   },
 };
