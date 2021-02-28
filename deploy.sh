bdcead89fae2b83b1e9c211849ad757424ab961c
Chems Eddine Senoussi <chemsedd@outlook.fr>
2021-02-28 21:59:27 +0100

    Updated icon and title

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:chemsedd/chemsedd.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chemsedd/chemsedd.github.io.git master:gh-pages
+# git push -f git@github.com:chemsedd/chemsedd-v2.git gh-pages
 
 cd -
